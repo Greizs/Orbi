93c2f0e4ed5ce3ef263380809a8ce08edc4e6dd2
Brandon Ibarra <brandelreyno@gmail.com>
2025-03-15 05:02:36 -0600

    reactnative y python estables

---
+++ frontend/App.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "#3B82F6" }}>
+      <Text style={{ color: "white", fontSize: 24 }}>¡Hola, Mundo! este es mi proyecto :D</Text>
+    </View>
+  );
+}
